73c568b3328b250473d3bfc950b0c27b94a85383
RB664 <ridhaabenjamin06@gmail.com>
2022-08-01 11:49:53 +0200

    Make it more better

--- courses.js
+++ courses.js
@@ -1,41 +1,61 @@
-const express = require('express')
-const port = parseInt(process.env.port) || 4000;
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+const port = parseInt(process.env.PORT) || 4040;
+
+const router = express.Router();
+
 let courses = [
     {
-        id: 1, 
-        name: 'Software Engineering'
+        id: 1, name: 'Software Engineering'
     },
     {
-        id: 2, 
-        name: 'Web Development'
+        id: 2, name: 'Web Development'
     },
     {
-        id: 3, 
-        name: "Database Management"
-    }    
+        id: 3, name: "Database Management"
+    }
 ];
 
-app.listen(port, ()=> {
-    console.log(`Server is running at port ${port}`);
-})
-app.get('/', (req, res)=> {
+app.use(router, cors(), express.json(), 
+    express.urlencoded({
+        extended: true
+    })
+);
+app.listen(port);
+
+router.get('^/$|/courses', (req, res)=> {
     res.send(courses);
 });
-app.get('/courses/:id', (req, res)=> {
+
+router.get('/courses/:id', (req, res)=> {
     const index = parseInt(req.params.id) - 1;
     if(index < courses.length) {
-        res.send(courses[index]);
+        res.status(200).json(
+            {result: courses[index]}
+        );
     }else {
-        res.send('Course was not found');
+        res.status(404).json(
+            {result: 'Resources was not found'}
+        );
     }
 });
+//
+router.post('/courses', bodyParser.json(), (req, res)=> {
+    const bd = req.body;
+    courses.push(
+        {
+            id: bd.id, name: bd.name
+        }
+    )
+    res.status(200).json(
+        {result: courses}
+    );
+});
 
-
-
-
-
-
-
-
-
-
+router.get('/*', (req, res)=> {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
